320160ddfe2e79c66c03b9401b5746aaaa378569
sergeytonal-design <sergeytonal@gmail.com>
2026-03-19 21:28:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+ import os
+from flask import Flask
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+app = Flask(__name__)
+
+telegram_app = Application.builder().token(BOT_TOKEN).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает на Render 🚀")
+
+telegram_app.add_handler(CommandHandler("start", start))
+
+@app.route("/")
+def home():
+    return "Bot is running!", 200
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
